3bd8b6990e5f84bb32edbf4e9a115b9ce937829d
Field Employee <fieldemployee@Fields-MBP.hsd1.ga.comcast.net>
2021-06-22 21:35:40 -0400

    added sharepreferences

--- app/src/main/java/com/dynamicdevz/chatmessenger/MainActivity.java
+++ app/src/main/java/com/dynamicdevz/chatmessenger/MainActivity.java
@@ -3,22 +3,36 @@ package com.dynamicdevz.chatmessenger;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.dynamicdevz.chatmessenger.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 private EditText textEditText;
 private Button sendButton;
 private TextView mainTV;
 private static final int REQUEST_CODE = 100;
+private SharedPreferences sharedPreferences;
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+        String value = sharedPreferences.getString("KEY", null);
+        if(value != null){
+            mainTV.setText(value);
+        }
+
+
     textEditText = findViewById(R.id.type_editview);
     sendButton = findViewById(R.id.send_button);
 
@@ -42,6 +56,9 @@ private static final int REQUEST_CODE = 100;
         if(requestCode == REQUEST_CODE){
             String dataString = data.getStringExtra("from");
             mainTV.setText(dataString);
+            SharedPreferences.Editor edit = sharedPreferences.edit();
+            edit.putString("KEY", dataString);
+            edit.apply();
         }
     }
 }
--- app/src/main/java/com/dynamicdevz/chatmessenger/NextActivity.java
+++ app/src/main/java/com/dynamicdevz/chatmessenger/NextActivity.java
@@ -2,19 +2,24 @@ package com.dynamicdevz.chatmessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
 
 public class NextActivity extends AppCompatActivity {
     private TextView textTV;
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
         textTV = findViewById(R.id.chat_textview);
+        sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+        String value = sharedPreferences.getString("KEY", null);
 
         Intent rI = getIntent();
         if(rI != null){
@@ -24,16 +29,21 @@ public class NextActivity extends AppCompatActivity {
             String fromActivity = text;
             textTV.setText(fromActivity);
 
+
             Intent dataIntent = new Intent();
             dataIntent.putExtra("from", fromActivity);
             setResult(RESULT_OK, dataIntent);
+            SharedPreferences.Editor edit = sharedPreferences.edit();
+            edit.putString("KEY", fromActivity);
+            edit.apply();
+
 
             new Handler(getMainLooper()).postDelayed(new Runnable(){
                 @Override
                 public void run() {
                     finish();
                 }
-            }, 3000);
+            }, 6000);
         }
     }
 }
